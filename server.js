1c3b2b861ae8a3c5d5bcd74c77a6b710789cf1a8
sbk2k1 <bhattacharyasaptarshi2001@gmail.com>
2021-12-20 10:35:56 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+const app = express()
+
+dotenv.config({path: './config.env'})
+const PORT = process.env.PORT || 8080
+
+app.get('/', (req, res) => {
+    res.send('Crud App')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${3000}`)
+})
